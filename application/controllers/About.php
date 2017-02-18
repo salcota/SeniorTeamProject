51e5bd65464fbda70b634aaf053a71706b35b7b0
kglchu <kglchu@mail.sfsu.edu>
2017-02-18 00:37:52 +0000

    Moed all AboutMe View pages to subfolder 'about' for categorization purposes. Might avoid a messy 'view' folder as we continue building the website.

--- application/controllers/About.php
+++ application/controllers/About.php
@@ -3,13 +3,13 @@
 class About extends CI_Controller {
 	public function view($member = "")
 	{
-		if (!file_exists(APPPATH.'views/' . $member . '.php'))
+		if (!file_exists(APPPATH.'views/about/' . $member . '.php'))
 		{
 			show_404();
 		}
 		else
 		{
-			$this->load->view($member);
+			$this->load->view('about/' . $member);
 		}
 	}
 }
