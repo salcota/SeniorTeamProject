f59fa74de5238d6f05168cea6b5d7acd11a6a0ec
--quiet <scota1@ip-172-31-19-160.us-west-1.compute.internal>
2017-02-21 00:05:32 +0000

    copied better php template

--- application/views/about/shane.php
+++ application/views/about/shane.php
@@ -1,8 +1,35 @@
-<html>
-  <head>
-  <title>About Shane</title>
-  </head>
-  <body>
-    <?php echo '<p>Hello World, from Shane</p>'; ?>
-  </body>
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+$this->load->helper('url');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    
+    <meta name="viewpoint" content="width = device-width, initial-scale = 1">
+    <title>About Shane</title>
+    
+    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    
+    <style>
+        .jumbotron
+        {
+            background-color:cadetblue;
+        }
+    </style>
+</head>
+<body>
+      
+   <div class="container">
+      <center><h1>Shane Cota</h1></center>
+      <center><img src="<?php echo base_url() . ""?>" alt="a pic at Mission Peak" height="300"/></center>
+           <center><p>Shane is a senior at SFSU, completing his bachelor's degree in Computer Science.</p></center>
+   </div>
+   
+    <script src="https://ajax.googeapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/bootstrap.min.js"></script>
+</body>
 </html>
