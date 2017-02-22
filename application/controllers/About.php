d19fa3bf385ae8115a1bf1a678265a30f83a5fb6
--quiet <sp17g04@ip-172-31-19-160.us-west-1.compute.internal>
2017-02-22 22:57:56 +0000

    Added display stating the website is an SFSU project on all pages

--- application/controllers/About.php
+++ application/controllers/About.php
@@ -4,6 +4,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class About extends CI_Controller {
 	public function view($member = "")
 	{
+		echo "<p style='text-align:center; font-style:italic; margin-bottom:25px'>SFSU Software Engineering Project, Spring 2017.  For Demonstration Only<p>";
+
 		if (!file_exists(APPPATH.'views/about/' . $member . '.php'))
 		{
 			show_404();
--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -20,6 +20,8 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
+		echo "<p style='text-align:center; font-style:italic; margin-bottom:25px'>SFSU Software Engineering Project, Spring 2017.  For Demonstration Only<p>";
+
 		$this->load->view('welcome_message');
 	}
 }
