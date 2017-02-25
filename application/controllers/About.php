1eff9a8fff5be39328b4da9464971015694ae9cd
--quiet <sp17g04@ip-172-31-19-160.us-west-1.compute.internal>
2017-02-25 00:45:58 +0000

    Added sfsu_demo header for all pages, added links to ihsan.php

--- application/controllers/About.php
+++ application/controllers/About.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class About extends CI_Controller {
 	public function view($member = "")
 	{
-		echo "<p style='text-align:center; font-style:italic; margin-bottom:25px'>SFSU Software Engineering Project, Spring 2017.  For Demonstration Only<p>";
+		$this->load->view('sfsu_demo');
 
 		if (!file_exists(APPPATH.'views/about/' . $member . '.php'))
 		{
