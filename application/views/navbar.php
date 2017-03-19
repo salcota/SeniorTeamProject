0b0fab88507a3dc453c714c44ec2f41da3575917
Kevin <kglchu@mail.sfsu.edu>
2017-03-19 04:08:25 +0000

    Navbar categories exception check Navbar will throw error if no categories are passed to it upon loading. This will help prevent mistakes as we build new controllers which utilize the Navbar view.

--- application/views/navbar.php
+++ application/views/navbar.php
@@ -1,6 +1,9 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 $this->load->helper('url');
+
+if (empty($categories))
+	throw new Exception('Navbar needs categories-listing from controller');
 ?>
     <body>
       
