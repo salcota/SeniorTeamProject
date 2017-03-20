cb263e8d29d506756d10f018e17d1b1e0dae11a4
Kevin <kglchu@mail.sfsu.edu>
2017-03-20 05:33:34 +0000

    Fixed home.php typo other students . . . who knows . . . students is plural. changed to: other students . . . who know . . .

--- application/views/home/home.php
+++ application/views/home/home.php
@@ -8,7 +8,7 @@ $this->load->helper('url');
     <h1 class="display-4">SFSU Congre-Gators</h1>
 
     <p class="lead">
-        Welcome to SFSU Congre-Gators, where SFSU students can buy and sell a variety of different items relevant to their needs. Shop anything from books, furniture, laptops, and much more from other students just like you,  who knows what its like to need that extra support to make it through college!
+        Welcome to SFSU Congre-Gators, where SFSU students can buy and sell a variety of different items relevant to their needs. Shop anything from books, furniture, laptops, and much more from other students just like you,  who know what its like to need that extra support to make it through college!
     </p>
 
     <hr class="my-4" >
