ea4ea2c309d14f19f82a4c617f568fc5a4f599bc
Mark <mtompong@mail.sfsu.edu>
2017-04-03 20:49:08 +0000

    Added contact.php & terms.php. Both connected to footerbar.php

---
+++ application/views/home/contact.php
@@ -0,0 +1,41 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+
+ 
+          <div class="container">
+              
+              <div class = "row justify-content-center pagetitle">
+                  <div class="col-12 subtitle" id="subheader">CONTACT</div>
+              </div>
+              
+              
+              <div class="jumbotron">
+		<center>
+             	<h1 class="display-3">Have any concerns? Contact Us!</h1>
+		<p class="lead"> Whether it's for technical issues, suggestions, or general feedback, please don't hesitate to reach out to our stuff.</p>
+		</center>
+		<hr class= "my-2">
+              </div>
+              
+             
+              
+              <div class="row justify-content-center">
+                  <div class="col-6">
+                      <h5>Phone: (415) - 265 - 3692</h5>
+		      <h5>Email: ihsan@mail.sfsu.edu</h5>
+		      <h5>Location: SFSU Congre-Gators Market, San Francisco, CA</h5>
+                  </div>
+              </div>
+              
+             </div>
+              
+             <br />
+              
+          
+
+	  <?php $this->load->view('common/jquery_tether_bootstrap'); ?>
+         
+  </body>
+
+</html>
