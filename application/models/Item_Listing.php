c0a1ec0d5c2359bdc05456ec6abe6590c1b637e2
Prateek Gupta <guptaprateek328@gmail.com>
2017-04-06 20:02:07 -0700

    debug code added

--- application/models/Item_Listing.php
+++ application/models/Item_Listing.php
@@ -128,7 +128,7 @@ class Item_Listing extends CI_Model
 			if (array_key_exists('listingID', $search)){
 
 			    $this->db->select('usr.username, item_listing.listing_id, item_listing.seller_id, item_listing.title, item_listing.description, item_listing.price, item_listing.posted_on, item_listing.display_pic, cat.category_name');
-                $this->db->from('item_listing');
+
 			    $this->db->join('reg_user usr', 'usr.user_id = item_listing.seller_id');
                 $this->db->join('item_category cat','cat.category_id = item_listing.category_id');
 			    $this->db->where('listing_id', $search['listingID']);
