04fa316b58c00e6768b378d26cac18197a0281b4
Prateek Gupta <guptaprateek328@gmail.com>
2017-04-06 20:21:00 -0700

    debug code added

--- application/controllers/Itemlisting.php
+++ application/controllers/Itemlisting.php
@@ -33,7 +33,10 @@ class Itemlisting extends CI_Controller{
             $search['listingID'] = $listingID;
             $item = $this->Item_Listing->getItems($search);
             $data['item'] = $item;
-            echo $item;
+            foreach ($item as $row)
+            {
+                echo $row->title;
+            }
             $item_pics = $this->Item_Listing->getAllItemListingPictures($listingID);
             $data['itemPics'] = $item_pics;
             $this->load->view('home/current_item',$data);
