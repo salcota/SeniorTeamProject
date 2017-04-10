1b38b2b3db96b8196bfde048e21571e046078364
ihsan-taha <ihsan@mail.sfsu.edu>
2017-04-10 07:12:43 +0000

    Adding completed final draft of about page

--- application/views/about/about.php
+++ application/views/about/about.php
@@ -27,16 +27,18 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
     <div class="row justify-content-center">
 
+	
 	<div class="col">
-        <div class="jumbotron" style="background-image: url(<?php echo base_url() . 'public/images/Unknown.jpeg'?>">
-              
+
+        <div class="jumbotron" style="color: #eee; background-image: url(<?php echo base_url() . 'public/images/san_francisco.png'?>">
+      
             <h1 class="display-4" style="text-align: center">Welcome to the Staff!</h1>
 
-	    <hr class="my-4">
+	    <hr class="my-4" style="background-color: #eee">
 
 	    <h5 style="text-align: center">CSC 648/848 - Software Engineering Class at SFSU</h5>
 
-	    <hr class="my-4">
+	    <hr class="my-4" style="background-color: #eee">
 
 	    <p class="lead">
 	        Spring 2017
@@ -57,24 +59,53 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	      <img src="http://www.ihsansdomain.com/IhsanTaha_ProfilePic.png" alt="Ihsan Taha"/>
 	      <br />
               <h5>Ihsan</h5>
-	      <p style="text-align: justify">I am a passionate programmer with a background in UI design and web development, but always open to learning new skills and adapting to new environments.</p>
+	      <p>CEO of SFSU Congre-Gator's Market, a passionate programmer with a background in UI design, web development, and game development.</p>
         </div>
 
         <div class="col-sm-4 about_us">
               <img src="https://scontent.fsnc1-5.fna.fbcdn.net/v/t31.0-8/15369961_1339774136074974_8835888128227889161_o.jpg?oh=8bc8dd4ff06a96bae490810799859836&amp;oe=59042FB6" alt="Prateek Gupta"/>
               <br />
               <h5>Prateek</h5>
-              <p style="text-align: justify">Master's Student Computer Science, Andorid Developer, Web App developer, Tech enthusiast</p>
+              <p>CTO of SFSU Congre-Gator's Market, Master's Student Computer Science, Andorid Developer, Web App developer, Tech enthusiast</p>
         </div>
 
         <div class="col-sm-4 about_us">
               <img src="<?php echo base_url() . "public/images/aboutDarel.jpg"?>" alt="a pic at Mission Peak"/>
               <br />
               <h5>Darel</h5>
-              <p style="text-align: justify">Darel is a senior at San Francisco State University, completing his bachelor's degree in Computer Science.</p>
+              <p>Darel is a senior at San Francisco State University, completing his bachelor's degree in Computer Science.</p>
         </div>
 
     </div>
+
+    <br />
+
+    <div class="row justify-content-center">
+
+        <div class="col-sm-4 about_us">
+              <img src="https://scontent-sjc2-1.xx.fbcdn.net/v/t1.0-9/15590368_10155476774979057_5985225702468684038_n.jpg?oh=2190452598d45fd86bfd543697207281&amp;oe=5948C81A" alt="Shane Cota"/>
+	      <br />
+              <h5>Shane</h5>
+              <p>Shane is a senior at San Francisco State University with an interest in network security, completing his bachelor's degree in Computer Science.</p>    
+	</div>
+
+        <div class="col-sm-4 about_us">
+              <img src="<?php echo base_url() . "public/images/aboutKevin.jpg"?>" alt="Kevin Chu"/>	
+	      <br />
+              <h5>Kevin</h5>
+              <p>Kevin is an experienced GitHub user and back-end developer.</p>
+	</div>
+
+        <div class="col-sm-4 about_us">
+              <img src="http://i.imgur.com/mKvvwpl.png" src="Mark Tompong"/>
+              <br />
+              <h5>Mark</h5>
+              <p>Mark is a senior in Computer Science and front-end developer of SFSU Congre-Gator's Market.</p>
+        </div>
+	
+    </div>
+
+    <br /><br /><br />
 
 </div>
 
