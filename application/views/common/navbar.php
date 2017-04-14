d280e375446da8b917446501f7d672e37cfb1584
ihsan-taha <ihsan@mail.sfsu.edu>
2017-04-14 23:12:50 +0000

    Incorporating logo to navbar

--- application/views/common/navbar.php
+++ application/views/common/navbar.php
@@ -14,7 +14,7 @@
         <button class='navbar-toggler navbar-toggler-right' type='button' data-toggle='collapse' data-target='#navbarSupportedContent' aria-controls='navbarSupportedContent' aria-expanded='false' aria-label='Toggle navigation'><span class='navbar-toggler-icon'></span></button>
 
 	<!-- Left Side Menu: Logo & Home-->
-        <a class='navbar-brand'>LOGO</a>
+        <a href='<?php echo base_url() . 'Home/view/home' ?>'><img style="background-color: #FFF; border-radius: 6px" src='<?php echo base_url() . 'public/images/logo.png'?>'></a>
 
         <div class='collapse navbar-collapse' id='navbarSupportedContent'>
             <ul class='navbar-nav mr-auto'>
