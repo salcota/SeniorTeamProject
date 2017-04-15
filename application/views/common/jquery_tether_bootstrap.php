f0358a9c48f8f115714554bb390d65e487ce49ff
Prateek Gupta <guptaprateek328@gmail.com>
2017-04-14 19:16:29 -0700

    Fixing UI

--- application/views/common/jquery_tether_bootstrap.php
+++ application/views/common/jquery_tether_bootstrap.php
@@ -1,19 +1,19 @@
 <!-- jQuery first, then Tether, then Bootstrap JS. -->
 <script src="<?php echo base_url() . '/public/js/jquery-3.1.1.min.js'?>">
 <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
-<script src="<?php echo base_url() . '/public/js/bootstrap.min.js'?>">
+<script src="<?php echo base_url() . '/public/js/bootstrap.min.js'?>"/>
 
 <script type="text/javascript">
     $(".category").click(function() {
         if ($(this).attr("id") == "default")
-	{
+	    {
             $("#btnGroupDrop1").html("All");
         }
         else 
         {
             $("#btnGroupDrop1").html($(this).html());
         }
-    })
+    });
  
     // test for uploading new photos on for the personal listings page
     $("#filechooser").click(function() 
@@ -23,7 +23,7 @@
                 $("#image" + i).attr('src', $source);
             alert($("#image" + i).attr("src"));
         }
-    })
+    });
 function readImageFile(input, id){
     if (input.files && input.files[0]) {
         var reader = new FileReader();
