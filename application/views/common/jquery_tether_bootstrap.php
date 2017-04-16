de5eea1478ede1b8c08765eacbbda0a21c219d14
--quiet <dogbonna@ip-172-31-19-160.us-west-1.compute.internal>
2017-04-16 11:04:33 +0000

    updating jquery.php file

--- application/views/common/jquery_tether_bootstrap.php
+++ application/views/common/jquery_tether_bootstrap.php
@@ -1,4 +1,14 @@
 <!-- jQuery first, then Tether, then Bootstrap JS. -->
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-97440164-1', 'auto');
+  ga('send', 'pageview');
+
+
 <script src="<?php echo base_url() . '/public/js/jquery-3.1.1.min.js'?>">
 <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
 <script src="<?php echo base_url() . '/public/js/bootstrap.min.js'?>">
@@ -24,25 +34,5 @@
             alert($("#image" + i).attr("src"));
         }
     })
-
-   (function(i,s,o,g,r,a,m)
-      {
-         i['GoogleAnalyticsObject']=r;
-         i[r]=i[r]||function()
-         {
-             (i[r].q=i[r].q||[]).push(arguments)
-         },
-         i[r].l=1*new Date();
-         a=s.createElement(o),
-         m=s.getElementsByTagName(o)[0];
-         a.async=1;
-         a.src=g;
-         m.parentNode.insertBefore(a,m)
-    })
-   
-   (window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-   ga('create', 'UA-97440164-1', 'auto');
-   ga('send', 'pageview');
 
 </script>
