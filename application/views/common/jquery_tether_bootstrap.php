5f815c47ecc84e10f5f2ece516db48ec9b434889
--quiet <dogbonna@ip-172-31-19-160.us-west-1.compute.internal>
2017-04-16 15:16:45 +0000

    updating jquery.php file for analytics tracking

--- application/views/common/jquery_tether_bootstrap.php
+++ application/views/common/jquery_tether_bootstrap.php
@@ -8,7 +8,7 @@
   ga('create', 'UA-97440164-1', 'auto');
   ga('send', 'pageview');
 
-
+</script>
 <script src="<?php echo base_url() . '/public/js/jquery-3.1.1.min.js'?>">
 <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
 <script src="<?php echo base_url() . '/public/js/bootstrap.min.js'?>">
