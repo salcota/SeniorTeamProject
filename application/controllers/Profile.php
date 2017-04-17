dc89c3fc840df5334f4a7ba58a64aa510c56d6f9
Kevin <kglchu@mail.sfsu.edu>
2017-04-17 23:29:23 +0000

    Re-added Mark's Profile controller

--- application/controllers/Profile.php
+++ application/controllers/Profile.php
@@ -4,8 +4,16 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Profile extends CI_Controller 
 {
 
+	function __construct() {
+	parent::__construct();
+	$this->load->database();
+	$this->load->library('loginhelper');
+	$this->load->model('Updateprofile');
+	}
+
 	public function me()
 	{
+
 		// Gets basic header and styles for all pages.
 		$this->load->view('common/sfsu_demo');
 		$this->load->view('common/required_meta_tags');
@@ -13,16 +21,24 @@ class Profile extends CI_Controller
 		// Loads No-Search Navbar.
 		$this->navbars->load();
 
-		// NEED TO IMPLEMENT USER DATA
-		$data = NULL;
-		
-		$this->load->view('profile/your_profile', $data);
+		// Returns necessary data for the view file (profile.php) to use
+		$user = $this->loginhelper->getLoginData();
+		$id = $user->user_id;
+
+		$data = array(
+			'username' =>$user->username,
+			'email' => $user->sfsu_email,
+			'biography'=> $user->biography
+
+		);
+
+		$this->load->view('profile/profile', $data);
 
 		// Gets basic footer and data that enables javascript, jQuery, and thether for all pages.
 		$this->load->view('common/jquery_tether_bootstrap');
 		$this->load->view('common/footerbar');
 	}
-	
+
 	public function user($id = NULL)
 	{
 		// Gets basic header and styles for all pages.
@@ -31,7 +47,7 @@ class Profile extends CI_Controller
 
 		// Loads No-Search Navbar.
 		$this->navbars->load();
-		
+
 		// NEED TO IMPLEMENT USER DATA
 		$data = NULL;
 
