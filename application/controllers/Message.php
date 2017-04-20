7b3e02ba1e79c9d3fc435d4c96babf8f474fc71b
Kevin <kglchu@mail.sfsu.edu>
2017-04-20 23:19:43 +0000

    Notification_Model updated with send/store functions.

--- application/controllers/Message.php
+++ application/controllers/Message.php
@@ -5,11 +5,18 @@ class Message extends CI_Controller {
 
 	public function __construct()
     	{
-			$this->load->model("Messages");
+			parent::__construct();
+			
+			// Verify the user is logged in.
+			if (!$this->loginhelper->isRegistered())
+				show_404();
+			
+			$this->load->model("Notification_Model");
     	}
 
 	public function index($page = "")
 	{
+		
 		
 	}
 	
--- application/models/Notification_Model.php
+++ application/models/Notification_Model.php
@@ -6,26 +6,51 @@ class Notification_Model extends CI_Model
 
     	public function __construct()
     	{
+			parent::__construct();
 			$this->load->database();
+			$this->load->model('Item_Listing');
     	}
 
-    	public function getAllNotifications($sender, $recv)
+    	public function getAllNotifications($buyer, $seller)
 		{
-			$result = $this->db->select("*")->from("reg_user_notification")->
-			group_start()->
-				where("sender_id", $sender)->
-				where("receiver_id", $recv)->
-			group_end()->
-			or_group_start()->
-				where("sender_id", $recv)->
-				where("receiver_id", $sender)->
-			group_end()->
-			get();
-			$result = $result->result();
-			return $result;
+			// Turn off debugging
+			$debug = $this->db->db_debug;
+			$this->db->db_debug = false;
+			
+			try
+			{
+				$this->db->join("item_listing", "item_listing.listing_id = reg_user_notification.listing_id");
+				$this->db->join("reg_user", "reg_user.user_id = item_listing.seller_id");
+				$data = $this->db->select("reg_user_notification.notification_id, reg_user_notification.sender_id, reg_user_notification.receiver_id, reg_user_notification.listing_id, reg_user_notification.message, reg_user_notification.status, reg_user.user_id, reg_user.username")->
+					group_start()->
+						group_start()->
+							where("sender_id", $buyer)->
+							where("receiver_id", $seller)->
+						group_end()->
+						or_group_start()->
+							where("sender_id", $seller)->
+							where("receiver_id", $buyer)->
+						group_end()->
+					group_end()->
+					group_start()->
+						where("reg_user.user_id", $seller)->
+					group_end()->
+				get("reg_user_notification")->result();
+			}
+			catch (Exception $e)
+			{
+				// Restore debug defaults
+				$this->db->db_debug = $debug;
+				throw new Exception("Error fetching Notifications");
+			}
+			
+			// Restore debug defaults
+			$this->db->db_debug = $debug;
+			
+			return $data;
 		}
 
-    	public function storeNotification($sender, $recv, $msg, $listing)
+    	public function storeNotification($sender, $recv, $listing, $msg)
 		{
 			// Turn off debugging
 			$debug = $this->db->db_debug;
@@ -43,9 +68,26 @@ class Notification_Model extends CI_Model
 			else
 			{
 				$this->db->db_debug = $debug;
-				throw new Exception("Cannot add user");
-				// More specific errors to be implemented later.
+				throw new Exception("Cannot add message");
 			}
+		}
+		
+		private function list2user($listing)
+		{
+			if (!is_numeric($listing))
+				throw new Exception("Listing ID is not numerical.");
+				
+			// Retrieve the listing
+			$search['listingID'] = $listing;
+			$data = $this->Item_Listing->getItems($search);
+			
+			// Check if the listing exists
+			if (count($data) < 1)
+				throw new Exception("Item Listing does not exist.");
+			else
+				$data = $data[0];
+			
+			return $data->username;
 		}
 }
 ?>
