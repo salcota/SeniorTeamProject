6a1ec30f42e05201714048b387e5b840b9a516bd
ihsan-taha <ihsan@mail.sfsu.edu>
2017-04-21 22:15:23 +0000

    Fixing container padding when navbar collapses

--- application/views/home/current_listing.php
+++ application/views/home/current_listing.php
@@ -2,7 +2,7 @@
 
 <?php $logged = $this->session->loggedIn; ?>
 
-<div class="container" style="margin-top: 77px">
+<div class="container">
 
     <!-- Notifies user that he or she is logged in if condition is true -->
     <p style="text-align: center">
