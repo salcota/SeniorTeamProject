1dce459c28e1b61c9fcb9ed76ba8ab93abe11134
Prateek Gupta <guptaprateek328@gmail.com>
2017-04-21 22:36:30 -0700

    added debug code in Itemlisting controller

--- application/controllers/Itemlisting.php
+++ application/controllers/Itemlisting.php
@@ -88,7 +88,7 @@ class Itemlisting extends CI_Controller
      * Saves an itemlisting with images
      */
     public function post_listing(){
-        $path = APPPATH . 'public/temp/';
+        $path = BASEPATH . 'public/temp/';
 
         try{
            /* if(!file_exists($path)){
