49f336ef52841a4b5ebede9912e481f85a4ba5ac
Prateek Gupta <guptaprateek328@gmail.com>
2017-04-22 13:33:18 -0700

    Debug code

--- application/controllers/Itemlisting.php
+++ application/controllers/Itemlisting.php
@@ -180,7 +180,7 @@ class Itemlisting extends CI_Controller
             array('required' => 'You must provide a %s.')
         );
         $this->form_validation->set_rules('description', 'Description of Item', 'trim|required|max_length[200]');
-
+        exit;
         if ($this->form_validation->run() == FALSE)
         {
             if(file_exists($this->uploadpath)){
@@ -193,7 +193,7 @@ class Itemlisting extends CI_Controller
             );
             $this->session->set_flashdata($data);
             print_r(($this->session->flashdata('item_form_errors')));
-            exit;
+
             redirect('add_item');
         }
         else
