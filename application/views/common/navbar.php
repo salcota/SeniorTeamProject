736a196060759bccef7297afb7baa74bd5649622
Mark <mtompong@mail.sfsu.edu>
2017-04-24 01:26:56 +0000

    preparing to attempt merge with beta

--- application/views/common/navbar.php
+++ application/views/common/navbar.php
@@ -1,7 +1,7 @@
 <?php
     defined('BASEPATH') OR exit('No direct script access allowed');
 
-    if (empty($categories))
+if (empty($categories))
         throw new Exception('Navbar needs categories-listing from controller');
 ?>
 
@@ -9,22 +9,22 @@
 
     <div id='topheader'></div>
 
-    <nav class='navbar navbar-toggleable-sm navbar-light fixed-top navbar' style="min-height: 82px">
+    <nav class='navbar navbar-toggleable-sm navbar-light fixed-top navbar' style="min-height: 65px">
 
-        <button style="border: solid 1px #696; border-radius: 6px" class='navbar-toggler navbar-toggler-right' type='button' data-toggle='collapse' data-target='#navbarSupportedContent' aria-controls='navbarSupportedContent' aria-expanded='false' aria-label='Toggle navigation'><i class="fa fa-bars" style="color: #EEE; font-size: 50px; height: 60px; padding-top: 5px; width: 60px" aria-hidden="true"></i></button>
+        <button style="border-style: none; cursor: pointer; margin-right: -10px" class='navbar-toggler navbar-toggler-right' type='button' data-toggle='collapse' data-target='#navbarSupportedContent' aria-controls='navbarSupportedContent' aria-expanded='false' aria-label='Toggle navigation'><i class="fa fa-bars" style="color: #EEE; font-size: 35px; padding-top: 7px; padding-right: -8px" aria-hidden="true"></i></button>
 
 	<!-- Left Side Menu: Logo & Home-->
-        <a href='<?php echo base_url() . 'Home/view/home' ?>'><img style= "background-color: #FFF; border-radius: 6px; height: 70px; width: 70px"  src='<?php echo base_url() . 'public/images/logo.png'?>'></a>
+        <a href='<?php echo base_url() . 'Home/view/home' ?>'><img style= "background-color: #FFF; border-radius: 6px; height: 55px; width: 55px"  src='<?php echo base_url() . 'public/images/logo.png'?>'></a>
 
         <div class='collapse navbar-collapse' id='navbarSupportedContent'>
             <ul class='navbar-nav mr-auto'>
                 <li class='nav-item active'>
-                    <a class="nav-link fix-align align-pt-16" style='width: 125px' href='<?php echo base_url() . 'Home/view/home' ?>'>Home</a>
+                    <a class="nav-link fix-align align-pt-16" style="width: 125px" href='<?php echo base_url() . 'Home/view/home' ?>'>Home</a>
                 </li>
             </ul>
 
             <!-- Centered Category Search & Input Search -->
-            <form class='form-inline mr-auto fix-align' style='padding-top: 4px' action="<?php echo base_url() . "home" ?>" id="searchSubmit" method=GET>
+            <form class='form-inline fix-align mr-auto' style='padding-top: 4px' action="<?php echo base_url() . "home" ?>" id="searchSubmit" method=GET>
                 <label class='sr-only' for='inlineFormInputGroup'>Search</label>
                     <div class='input-group mb-2 mr-sm-2 mb-sm-0'>
                         <div class='form-group'>
@@ -37,7 +37,6 @@
                                     $selector = "";
                                     if ($currentCategory == $category->category_id)
                                         $selector = " selected=\"selected\"";
-
                                     // Adds category option to list
                                     print "<option value=$category->category_id$selector>$category->category_name</option>\r\n";
                                 }
@@ -47,14 +46,14 @@
                     <input style='height: 40px' type='search' class='form-control' id='inlineFormInputGroup' placeholder='Search ...'
                        name='search' value="<?php echo $searchTerms ?>">
                     <input type='hidden' name='sort' id='sort'>
-                    <button class='btn btn-success' style='height: 40px' type='submit'><i class='fa fa-search' aria-hidden='true'></i></button>
+                    <button class='btn btn-success' style='cursor: pointer; height: 40px' type='submit'><i class='fa fa-search' aria-hidden='true'></i></button>
                 </div>
             </form>
 
             <!-- Right Side Menu: Sell, Sign Up, and Login -->
             <ul class='navbar-nav'>
                 <li class='nav-item'>
-                    <a class='nav-link fix-align align-pt-16' href='<?php echo base_url() . 'Home/view/edit_listing'?>'>Sell</a>
+                    <a class='nav-link fix-align align-pt-16' href='<?php echo base_url() . 'add_item'?>'>Sell</a>
                 </li>
                 <li class='nav-item'>
                     <a class='nav-link fix-align align-pt-16' href='<?php echo base_url() . 'Home/view/signup'?>'>Sign&nbsp;Up</a>
