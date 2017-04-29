03394ba4e423918aefe631c8bf00ba6af245bec2
ihsan-taha <ihsan@mail.sfsu.edu>
2017-04-29 16:17:49 +0000

    Fixing modal consistency for home page (changed Meetup to Meet-up)

--- application/views/home/home.php
+++ application/views/home/home.php
@@ -172,11 +172,11 @@ Welcome to SFSU Congre-Gators, where SFSU students can buy and sell a variety of
 		</div>
 
                 <div class="modal-footer">
-		   <span style="width: 55%">Date:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <?php echo "March 10, 2017"; ?>
-		   <br /><?php $location = 'Spot 1 - Quad'; echo 'Meetup: ' . $location; ?></span>
-		   <a class="btn btn-secondary btn-sm" href="<?php echo base_url() . 'Home/view/googlemaps_test'?>">View Map</a>
-                   <button type="button" class="btn  btn-secondary btn-sm" style="cursor: pointer" data-dismiss="modal">Close</button>
-                   <?php
+                    <span style="width: 100%">Date:&nbsp;&nbsp;&emsp;&emsp; <?php echo "March 10, 2017"; ?>
+                    <br /><?php $location = 'Spot 1 - Quad'; echo 'Meet-up:<span>&emsp;</span>' . $location; ?></span>
+                    <a class="btn btn-secondary btn-sm" href="<?php echo base_url() . 'Home/view/googlemaps_test'?>">View Map</a>
+                    <button type="button" class="btn  btn-secondary btn-sm" style="cursor: pointer" data-dismiss="modal">Close</button>
+                    <?php
                         $data = array(
                             'class'         => 'btn btn-success btn-sm',
                             'name'          => 'submit',
