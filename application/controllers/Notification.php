c9dd1fcbb341a1dbab26e1ef85cb045ebc554747
Kevin <kglchu@mail.sfsu.edu>
2017-04-30 03:32:12 +0000

    Removed Dummy Notification users.

--- application/controllers/Notification.php
+++ application/controllers/Notification.php
@@ -54,8 +54,6 @@ class Notification extends CI_Controller
 			if ($i < count($buyers) - 1)
 				echo "\r\n\r\n";
 		}
-		
-		echo "Petkovic\r\n777";
 	}
 	
 	public function getSellers()
@@ -73,7 +71,6 @@ class Notification extends CI_Controller
 			if ($i < count($sellers) - 1)
 				echo "\r\n\r\n";
 		}
-		echo "Petkovic\r\n777";
 	}
 	
     public function get_all_notifications($partnerID, $partnerIsSeller)
