3bfb19b6b209f6772e7595e7772cf6d39bc58aef
Prateek Gupta <guptaprateek328@gmail.com>
2017-05-09 01:51:01 -0700

    fixed image issue in about page

--- application/views/about/about.php
+++ application/views/about/about.php
@@ -46,7 +46,7 @@
 
 	    <div class="col-sm-4 about_us">
 	        <div class="about_frame">
-                    <img src="https://scontent.fsnc1-5.fna.fbcdn.net/v/t31.0-8/15369961_1339774136074974_8835888128227889161_o.jpg?oh=8bc8dd4ff06a96bae490810799859836&amp;oe=59042FB6" alt="Prateek Gupta"/>
+                    <img src="<?php echo base_url() . "public/images/aboutPrateek.jpg"?>" alt="Prateek Gupta"/>
 	        </div>
                 <br />
                 <h5>Prateek</h5>
